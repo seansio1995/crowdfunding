8ec2733d85800d4a8cacf85abfd3aa03accc900b
dmm7cx <31854126+dmm7cx@users.noreply.github.com>
2017-11-05 16:09:54 -0500

    Update models.py

--- cs3240-f17-team20/demo/demo/catalog/models.py
+++ cs3240-f17-team20/demo/demo/catalog/models.py
@@ -2,37 +2,82 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
-class Person(models.Model):
-    Name = models.CharField(max_length=20, help_text="Enter a name")
+class Company(models.Model):
+    Username = models.CharField(max_length=20, help_text="Enter a username")
 
-    Age = models.IntegerField(help_text="Enter an age")
+    Company = models.IntegerField(help_text="Enter a Company Name")
 
     class Meta:
-        ordering = ["Name"]
+        ordering = ["Company"]
+
+    def __str__(self):
+        return self.Username
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular instance of the model.
+        """
+        return reverse('model-detail-view', args=[str(self.id)])
+
+
+class Intevestor(models.Model):
+    UserName = models.CharField(max_length=20, help_text="Enter a username")
+
+
+    class Meta:
+        ordering = ["UserName"]
+
+    def __str__(self):
+        return self.UserName
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular instance of the model.
+        """
+        return reverse('model-detail-view', args=[str(self.id)])
+
+
+class Manager(models.Model):
+    UserName = models.CharField(max_length=20)
+
+    class Meta:
+        ordering = ["UserName"]
+
+    def __str__(self):
+        return self.UserName
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular instance of the model.
+        """
+        return reverse('model-detail-view', args=[str(self.id)])
+
+class Report(models.Model):
+    company = models.CharField(max_length=30, help_text="Company name")
+
+    phone = models.CharField(max_length=20, help_text="Company phone number")
+
+    location = models.CharField(max_length=20, help_text="Company location")
+
+    country = models.CharField(max_length=30, help_text="Company country")
+
+    sector = models.CharField(max_length=30, help_text="Company sector")
+
+    industry = models.CharField(max_length=30, help_text="Company industry")
+
+    class Meta:
+        ordering = ["company"]
 
     def __str__(self):
         return self.Name
 
     def get_absolute_url(self):
-        """
-        Returns the url to access a particular instance of the model.
-        """
         return reverse('model-detail-view', args=[str(self.id)])
 
 
-class Friend(models.Model):
-    Name = models.CharField(max_length=20, help_text="Enter a name")
 
-    Age = models.IntegerField(help_text="Enter an age")
 
-    class Meta:
-        ordering = ["Name"]
 
-    def __str__(self):
-        return "The friend" + self.Name
 
-    def get_absolute_url(self):
-        """
-        Returns the url to access a particular instance of the model.
-        """
-        return reverse('model-detail-view', args=[str(self.id)])
+
+
