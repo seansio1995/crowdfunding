eec49b0e05bdfa25b25ea3182db6f6972d45cf42
dmm7cx <31854126+dmm7cx@users.noreply.github.com>
2017-11-05 16:17:33 -0500

    Update admin.py

--- demo/demo/catalog/admin.py
+++ demo/demo/catalog/admin.py
@@ -1,11 +1,2 @@
 from django.contrib import admin
 from .models import Person, Friend, Report
-# Register your models here.
-admin.site.register(Person)
-admin.site.register(Friend)
-#admin.site.register(Report)
-
-@admin.register(Report)
-class ReportAdmin(admin.ModelAdmin):
-    pass
-
