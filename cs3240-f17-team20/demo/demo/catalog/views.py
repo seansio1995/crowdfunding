15fa768fcaf8ac8831a31492414dd31447018e55
Shanya Hans <sh3kj@virginia.edu>
2017-11-05 15:42:17 -0500

    Update views.py

--- cs3240-f17-team20/demo/demo/catalog/views.py
+++ cs3240-f17-team20/demo/demo/catalog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
 from .models import Person, Friend
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 # Create your views here.
 
 def index(request):
@@ -11,3 +13,20 @@ def index(request):
         'index.html',
         context={'num_peeps':num_peeps,'num_frens':num_frens},
     )
+
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
