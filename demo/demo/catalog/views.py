5e1699280cfd06758467fa7a3525a35cf621d110
dmm7cx <31854126+dmm7cx@users.noreply.github.com>
2017-11-06 19:56:45 -0500

    Add files via upload

--- demo/demo/catalog/views.py
+++ demo/demo/catalog/views.py
@@ -44,3 +44,20 @@ def Login(request):
 
 def logout(request):
     return render(request, 'logged_out.html')
+
+
+def messaging(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm
+    return render(request, 'messaging.html', {'form': form})
+
+
