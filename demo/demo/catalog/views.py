c4c9154af01e2ff9234d16fb55d75e7460c8a957
Shanya Hans <sh3kj@virginia.edu>
2017-11-06 19:47:33 -0500

    Update views.py

--- demo/demo/catalog/views.py
+++ demo/demo/catalog/views.py
@@ -51,3 +51,17 @@ def logout(request):
 
 def createreport(request):
     
+def messaging(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm
+    return render(request, 'messaging.html', {'form': form})
+
