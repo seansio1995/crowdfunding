fe9c141e4b032c5af5d2cf8ec9eb73c4c8df29e2
dmm7cx <31854126+dmm7cx@users.noreply.github.com>
2017-11-13 16:31:10 -0500

    Add files via upload

---
+++ cs3240-f17-team20/demo/demo/catalog/urls.py
@@ -0,0 +1,29 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.contrib.auth.views import logout
+
+
+
+
+urlpatterns = [
+    url(r'^reports/', views.report_list, name='reports'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^$', views.index, name='index'),
+    url(r'^login/$', views.Login, name = 'login'),
+    url(r'^messaging/$', views.messaging, name = 'messaging'),
+    url(r'^logout/$', logout, {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
+    url(r'^createreport/$', views.createreport, name='createreport'),
+    url(r'^viewreport/$', views.viewreport, name='viewreport'),
+    url(r'^viewallreport/$', views.viewallreport, name='viewallreport'),
+    url(r'^loggedin/$', views.user_home, name='userhome'),
+    url(r'^createG/$', views.create_group, name='create-group'),
+    url(r'^addGuser/$', views.add_user_to_group, name='add-user-g'),
+    url(r'^userG/$', views.group_list, name='Glist'),
+    url(r'^manager/$', views.manager_home, name='managerhome'),
+    url(r'^addsm/$', views.add_SM, name='addSM'),
+    url(r'^deleteuser/$', views.delete_user, name='delete-user-g'),
+    url(r'^adderror/$', views.add_error, name='add-error'),
+    url(r'^Suspend/$', views.suspend_user, name='suspend'),
+    url(r'^UnSuspend/$', views.unsuspend_user, name='unsuspend'),
+]
