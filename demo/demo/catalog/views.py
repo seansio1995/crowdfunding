7b39cb4b51c9560cc98bc884253b32cfd4c447e4
Shanya Hans <sh3kj@virginia.edu>
2017-11-18 14:28:02 -0500

    Update views.py

--- demo/demo/catalog/views.py
+++ demo/demo/catalog/views.py
@@ -316,7 +316,7 @@ def send_message(request):
     
 #@csrf_protect
 def delete_message(request):
-    if(request.GET.get('mybtn')):
+    if(request.GET.get('delete')):
         messages = Message.objects.all()
         for message in messages: 
             message.delete()
