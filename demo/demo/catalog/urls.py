f383e199842097976a1b0058a6b8066e418393b6
Shanya Hans <sh3kj@virginia.edu>
2017-11-20 10:12:59 -0500

    Update urls.py

--- demo/demo/catalog/urls.py
+++ demo/demo/catalog/urls.py
@@ -32,6 +32,6 @@ urlpatterns = [
     url(r'^UnSuspend/$', views.unsuspend_user, name='unsuspend'),
     url(r'^sendmessage/$',views.send_message,name="sendmessage"),
     url(r'^receivemessage/$',views.receive_message,name="receivemessage"),
-    url(r'^deletemessage/$', views.deletemessage, name='deletemessage'),
+    url(r'^(?P<pk>[0-9]+)/$', views.deletemessage, name = 'deletemessage'),
     url(r'^gohome/$',views.gohome,name="gohome"),
 ]
